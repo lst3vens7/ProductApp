3962c074a0483491c81a8dcce9e63f0047941dd8
lst3vens7 <145001217+lst3vens7@users.noreply.github.com>
2023-10-04 14:34:43 +0100

    Add project files.

---
+++ ProductApp/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProductApp.Models;
+using System.Data.Entity;
+
+namespace ProductApp.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set;}
+    }
+}
